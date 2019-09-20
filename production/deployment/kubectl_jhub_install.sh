67e6ab5b73d876719f33f4853e8810daed2bbd6a
Mohammad Wasil <mwasil@outlook.co.id>
2019-09-20 21:08:55 +0700

    change jhub version

--- production/deployment/kubectl_jhub_install.sh
+++ production/deployment/kubectl_jhub_install.sh
@@ -1,6 +1,6 @@
 #openssl rand -hex 32
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
-RELEASE=prod
-NAMESPACE=prod
-helm upgrade --install $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.7.0 --values config/config.yaml
+RELEASE=testing
+NAMESPACE=testing
+helm upgrade --install $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.8.2 --values config/config_empty.yaml
--- production/deployment/kubectl_update_only.sh
+++ production/deployment/kubectl_update_only.sh
@@ -1,4 +1,5 @@
 helm repo update
-RELEASE=prod
-NAMESPACE=prod
-helm upgrade $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.7.0 --values config/config.yaml
+RELEASE=testing
+NAMESPACE=testing
+#helm upgrade $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.7.0 --values config/config.yaml
+helm upgrade $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.8.2 --values config/config_empty.yaml
